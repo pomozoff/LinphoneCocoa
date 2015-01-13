95e1e905cf4d0386eb1c8d69b350431ec361645e
Anton Pomozov <pomozoff@gmail.com>
2015-01-13 22:32:27 +0300

    [Mod]: Code has been commented to use in future

--- LinphoneCocoaTests/AKDLinphoneCoreTests.m
+++ LinphoneCocoaTests/AKDLinphoneCoreTests.m
@@ -36,11 +36,22 @@
     [self waitForExpectationsWithTimeout:1 handler:nil];
 }
 
+/*
+
+ [self waitForExpectationsWithTimeout:1
+ handler:^(NSError *error) {
+ // handler is called on _either_ success or failure
+ if (error != nil) {
+ XCTFail(@"timeout error: %@", error);
+ }
+ }];
+ 
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
     }];
 }
+*/
 
 @end
